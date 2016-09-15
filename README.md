dea72f09f34bccf475323eaa2c6c3476e04a1a52
Pixelastic <tim@pixelastic.com>
2016-09-15 17:09:31 +0200

    Adding -dimensions, -width and -height methods

--- README.md
+++ README.md
@@ -23,9 +23,24 @@ This repository holds a bunch of command line scripts I use to manipulate images
   Will convert any number of GIF images into their PNG versions. Note that if
   the GIF is animated, the first frame will be extracted instead.
 
+### Modification
+
+#### resize
+  Will resize the input files to the specified dimensions. If one dimension
+  is given, the other will be resized to keep the same image ratio
+
 ### Information
 
 #### gif-is-animated
   Check if the input file is an animated GIF or not
+
+#### img-width
+  Return the width of the file
+
+#### img-height
+  Return the height of the file
+
+#### img-dimensions
+  Return the width x height of the file
 
 
--- lib/image_helper.rb
+++ lib/image_helper.rb
@@ -3,6 +3,14 @@ require 'shellwords'
 
 # Allow access to current git repository state
 module ImageHelper
+  # Check if the input is an image
+  def image?(input)
+    input = File.expand_path(input)
+    return false unless File.exist?(input)
+    allowed_extensions = %w(.gif .png .jpg)
+
+    allowed_extensions.include?(File.extname(input))
+  end
 
   # Check that all the input exists and are of the current type
   def validate_inputs(inputs, extension)
@@ -36,6 +44,18 @@ module ImageHelper
     basename = File.basename(input, File.extname(input))
     output = File.join(dirname, "#{basename}.#{extension}")
     `convert #{input.shellescape} #{output.shellescape}`
+  end
+
+  # Returns the width of the input
+  def width(input)
+    raw = `exiftool -ImageWidth #{input.shellescape}`
+    raw.split(':')[-1].to_i
+  end
+
+  # Returns the height of the input
+  def height(input)
+    raw = `exiftool -ImageHeight #{input.shellescape}`
+    raw.split(':')[-1].to_i
   end
 
 end

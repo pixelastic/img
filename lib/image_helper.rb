74c983562e86e3c9a19de069b46763104678b0c8
Pixelastic <tim@pixelastic.com>
2016-09-15 16:30:12 +0200

    jpg2png

--- lib/image_helper.rb
+++ lib/image_helper.rb
@@ -1,17 +1,25 @@
+require 'awesome_print'
+require 'shellwords'
+
 # Allow access to current git repository state
 module ImageHelper
 
+  # Check that all the input exists and are of the current type
   def validate_inputs(inputs, extension)
+    # Using full path
     inputs = inputs.map do |input|
       File.expand_path(input)
     end
 
+    # Rejecting non-existing files and files of the wrong extension
     inputs = inputs.reject do |input|
       next true unless File.exist?(input)
-      next true unless File.extname(input).downcase == extension
+      next true unless File.extname(input).downcase == ".#{extension}"
+      ap input
       false
     end
 
+    # Displaying error and stopping
     if inputs.empty?
       puts 'Usage:'
       puts @usage
@@ -19,6 +27,16 @@ module ImageHelper
       puts @documentation
       exit 1
     end
+
+    inputs
+  end
+
+  # Convert the specified to the specified extension
+  def convert(input, extension)
+    dirname = File.dirname(input)
+    basename = File.basename(input, File.extname(input))
+    output = File.join(dirname, "#{basename}.#{extension}")
+    `convert #{input.shellescape} #{output.shellescape}`
   end
 
 end

1270906eb9f5663cfa0c8665274d93e72679e602
Pixelastic <tim@pixelastic.com>
2016-12-06 22:30:44 +0100

    Trim PNG by removing whitespace around them

--- lib/png_helper.rb
+++ lib/png_helper.rb
@@ -4,4 +4,15 @@ module PngHelper
   def png?(input)
     File.extname(input).casecmp('.png').zero?
   end
+
+  # Trim the image, removing whitespace around it
+  def trim(input)
+    options = [
+      input.shellescape,
+      '-trim',
+      input.shellescape
+    ]
+    command = "convert #{options.join(' ')}"
+    `#{command}`
+  end
 end
--- spec/png_helper_spec.rb
+++ spec/png_helper_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe(PngHelper) do
-  let(:test_instance) { Class.new { include ImageHelper }.new }
+  let(:t) { Class.new { include ImageHelper }.new }
   let(:png) { fixture('png/dices.png') }
+  let(:aasimar) { fixture('png/aasimar.png') }
 
   describe 'png?' do
     it 'returns true if file is a png' do
@@ -10,10 +11,30 @@ describe(PngHelper) do
       input = png
 
       # When
-      actual = test_instance.png?(input)
+      actual = t.png?(input)
 
       # Then
       expect(actual).to eq true
     end
   end
+
+  describe 'trim' do
+    it 'should remove whitespace around the image' do
+      # Given
+      input = copy(aasimar)
+      before_width = t.width(input)
+      before_height = t.height(input)
+
+      # When
+      t.trim(input)
+      after_width = t.width(input)
+      after_height = t.height(input)
+
+      # Then
+      expect(before_width).to be > after_width
+      expect(before_height).to be > after_height
+    end
+  end
+
+
 end
